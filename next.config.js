9022a2210840358cdc44af868936568054a7a3b5
Yujin Im <jina0605@gmail.com>
2023-05-19 02:58:26 +0900

    Add CD

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = { output: "export", basePath: "/danfung-setlist" };
 
-module.exports = nextConfig
+module.exports = nextConfig;
