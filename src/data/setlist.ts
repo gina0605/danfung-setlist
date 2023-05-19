94160c30bccb37b972c06dc2cd7aab713f6d4972
Yujin Im <jina0605@gmail.com>
2023-05-19 22:40:25 +0900

    Add AAUSIC session

---
+++ src/data/setlist.ts
@@ -0,0 +1,250 @@
+import { song } from "@/types";
+
+export const bands = ["단풍", "더블에이뮤직"];
+
+export const setlist: song[][] = [
+  [
+    {
+      title: "한낮",
+      composer: "쏜애플",
+      session: ["V 임한비", "G 박조현 한석희", "B 신원직", "D 송석빈"],
+    },
+    {
+      title: "Plug In Baby",
+      composer: "Muse",
+      session: [
+        "V 김창덕",
+        "G 이재만 한석희",
+        "B 신원직",
+        "K 여창민",
+        "D 송석빈",
+      ],
+    },
+    {
+      title: "낯선 열대",
+      composer: "쏜애플",
+      session: ["V 남유주", "G 이주미 이재만", "B 송석빈", "D 임유진"],
+    },
+    {
+      title: "로마네스크",
+      composer: "쏜애플",
+      session: [
+        "V 최현서",
+        "G 한석희 이주미",
+        "B 송석빈",
+        "K 여창민",
+        "D 임유진",
+      ],
+    },
+    {
+      title: "KICK BACK",
+      composer: "Yonezu Kenshi",
+      session: [
+        "V 남유주",
+        "G 박조현",
+        "B 신원직",
+        "K 여창민 김세은",
+        "D 임유진",
+      ],
+    },
+    {
+      title: "Urban Lifestyle",
+      composer: "데이브레이크",
+      session: [
+        "V 임한비",
+        "G 이주미",
+        "B 신원직",
+        "K 여창민 김세은",
+        "D 임유진",
+      ],
+    },
+    {
+      title: "Bad Liar",
+      composer: "Imagine Dragons",
+      session: [
+        "V 김창덕",
+        "G 박조현 이재만",
+        "B 신원직",
+        "K 김세은 여창민",
+        "D 임유진",
+      ],
+    },
+    {
+      title: "기억을 걷는 시간",
+      composer: "넬",
+      session: [
+        "V 최현서",
+        "G 이재만 한석희",
+        "B 송석빈",
+        "K 김세은 여창민",
+        "D 임유진",
+      ],
+    },
+  ],
+  [
+    {
+      title: "Where the Streets Have No Name",
+      composer: "U2",
+      session: ["V 김창덕", "G 이지수", "B 정동운", "K 이진주", "D 서동희"],
+    },
+    {
+      title: "I Knew You Were Trouble",
+      composer: "Taylor Swift",
+      session: [
+        "V 은지원",
+        "G 김병수 이지수",
+        "B 정동운",
+        "K 이진주",
+        "D 서동희",
+      ],
+    },
+    {
+      title: "Make Me Wanna Die",
+      composer: "The Pretty Reckless",
+      session: [
+        "V 신현진",
+        "G 김병수 김현수",
+        "B 최석영",
+        "K 이승진",
+        "D 김민주",
+      ],
+    },
+    {
+      title: "얼음요새",
+      composer: "터치드",
+      session: [
+        "V 이지인",
+        "G 김병수 김현수",
+        "B 정동운",
+        "K 이승진",
+        "D 서동희",
+      ],
+    },
+    {
+      title: "사랑하긴 했었나요",
+      composer: "잔나비",
+      session: [
+        "V 은지원",
+        "G 김병수 김현수",
+        "B 정동운",
+        "K 이승진",
+        "D 서동희",
+      ],
+    },
+    {
+      title: "빗속에서",
+      composer: "YB",
+      session: [
+        "V 김창덕",
+        "G 김우재 김현수",
+        "B 최석영",
+        "K 이진주",
+        "D 서동희",
+      ],
+    },
+    {
+      title: "금지된 사랑",
+      composer: "김경호",
+      session: [
+        "V 신현진",
+        "G 김우재 김현수",
+        "B 최석영",
+        "K 이승진, 이진주",
+        "D 김민주",
+      ],
+    },
+    {
+      title: "LETTER",
+      composer: "유다빈밴드",
+      session: [
+        "V 이지인",
+        "G 김우재 김현수",
+        "B 최석영",
+        "K 이승진",
+        "D 김민주",
+      ],
+    },
+    {
+      title: "Betelgeuse",
+      composer: "Yuuri",
+      session: [
+        "V 김창덕",
+        "G 김우재 이지수",
+        "B 최석영",
+        "K 이진주",
+        "D 김민주",
+      ],
+    },
+    {
+      title: "Blue",
+      composer: "터치드",
+      session: [
+        "V 은지원",
+        "G 김우재 이지수",
+        "B 정동운",
+        "K 이진주",
+        "D 서동희",
+      ],
+    },
+    {
+      title: "popo",
+      composer: "백예린",
+      session: ["V 신현진", "G 김병수", "B 정동운", "K 이승진", "D 김민주"],
+    },
+    {
+      title: "サムライハート(Some Like it Hot)",
+      composer: "SPYAIR",
+      session: [
+        "V 이지인",
+        "G 김병수 이지수",
+        "B 정동운",
+        "K 이승진",
+        "D 김민주",
+      ],
+    },
+    {
+      title: "9",
+      composer: "Silica Gel",
+      session: [
+        "V 김창덕",
+        "G 김병수 김현수",
+        "B 최석영",
+        "K 이승진",
+        "D 김민주",
+      ],
+    },
+    {
+      title: "good 4 u",
+      composer: "Olivia Rodrigo",
+      session: [
+        "V 은지원",
+        "G 김우재 김현수",
+        "B 최석영",
+        "K 이진주",
+        "D 서동희",
+      ],
+    },
+    {
+      title: "Alive",
+      composer: "터치드",
+      session: [
+        "V 신현진",
+        "G 김우재 이지수",
+        "B 최석영",
+        "K 이진주",
+        "D 김민주",
+      ],
+    },
+    {
+      title: "Sk8er Boi",
+      composer: "Avril Lavigne",
+      session: [
+        "V 이지인",
+        "G 김우재 이지수",
+        "B 정동운",
+        "K 이진주",
+        "D 서동희",
+      ],
+    },
+  ],
+];
